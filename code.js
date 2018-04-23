bc33b807cba6a7db4e57798b7d22497ed0253a13
Erik Nordland <eriknordland@gmail.com>
2018-04-23 12:05:34 -0700

    I Do Declare by Erik Nordland final

---
+++ code.js
@@ -0,0 +1,23 @@
+function Declaration(){
+    var firstNum = 5;
+    var secondNum = 2;
+    var thirdNum = 8;
+    var fourthNum = 779;
+    var firstString = 'woo';
+    var secondString = 'uhhh';
+    var thirdString = 'aklfbh';
+    var firstbool = true;
+    var secondbool = false;
+    var WaltWhitman;
+    console.log("The First int:", firstNum);
+    console.log("The second int:", secondNum);
+    console.log("The third int:", thirdNum);
+    console.log("The fourth int:", fourthNum);
+    console.log("The First String:", firstString);
+    console.log("The second String:", secondString);
+    console.log("The third String:", thirdString);
+    console.log("The First Boolean:", firstbool);
+    console.log("The second boolean:", secondbool);
+    console.log("undefined", WaltWhitman);
+}
+Declaration();
